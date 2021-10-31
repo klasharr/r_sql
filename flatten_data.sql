6ba600c4d4b7d44638990fa127e323ae08b78f29
Klaus Harris <klasharr@users.noreply.github.com>
2021-10-31 18:27:16 +0000

    Update flatten_data.sql

--- flatten_data.sql
+++ flatten_data.sql
@@ -15,27 +15,7 @@ SELECT
     e.rank as series_rank,
     e.final_score as series_score,
     ra.date,
-    cl.name as club_name,
-    md5(concat( 
-        first_name, 
-        last_name,
-        c.id,
-        cs.name,
-        cr.identifier,
-        f.name,
-        s.name,
-        ra.name,
-        r.corrected_time,
-        r.status,
-        r.elapsed_time,
-        r.discarded,
-        r.points,
-        e.rank,
-        e.final_score,
-        r.laps,
-        ra.date,
-        cl.name
-        )) as row_md5
+    cl.name as club_name
 FROM `results` r 
     join entries e on r.entry_id = e.id 
     join competitors c on c.id = e.competitor_id
@@ -45,8 +25,3 @@ FROM `results` r
     join classifications cs on cs.id = e.classification_id 
     join crafts cr on cr.id = r.craft_id
     join fleets f on f.id = e.fleet_id
-where 
-     s.name = 'Summer Sunday Series 2021' and 
-     ra.name = 'Race 2' AND
-     f.name = 'ILCA' AND
-     r.status not in ('DNC', 'OOD')
