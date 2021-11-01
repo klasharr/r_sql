dff6740ff997838e4809eda0cbd41c5f3e41cfa6
Klaus Harris <klasharr@users.noreply.github.com>
2021-11-01 07:38:58 +0000

    Update flatten_data.sql

--- flatten_data.sql
+++ flatten_data.sql
@@ -1,28 +1,55 @@
-SELECT 
-    first_name, 
-    last_name,
-    cs.name as class,
-    cr.identifier as sail_number,
-    f.name as fleet_name,
-    s.name as series_name,
-    ra.name as race_name,
-    r.elapsed_time,
-    r.corrected_time,
-    r.laps,
-    r.status,
-    r.discarded,
-    r.points,
-    r.id as race_id,
-    e.rank as series_rank,
-    e.final_score as series_score,
-    ra.date,
-    cl.name as club_name
-FROM `results` r 
-    join entries e on r.entry_id = e.id 
-    join competitors c on c.id = e.competitor_id
-    join races ra on ra.id = r.race_id
-    join series s on s.id = ra.series_id
-    join clubs cl on cl.id = s.club_id
-    join classifications cs on cs.id = e.classification_id 
-    join crafts cr on cr.id = r.craft_id
-    join fleets f on f.id = e.fleet_id
+
+    SELECT
+        `c`.`first_name` AS `first_name`,
+        `c`.`last_name` AS `last_name`,
+        CONCAT(`c`.`first_name`, `c`.`last_name`) AS `name`,
+        `cs`.`name` AS `class`,
+        `cr`.`identifier` AS `sail_number`,
+        `f`.`name` AS `fleet_name`,
+        `s`.`name` AS `series_name`,
+        `ra`.`name` AS `race_name`,
+        `r`.`elapsed_time` AS `elapsed_time`,
+        `r`.`corrected_time` AS `corrected_time`,
+        `r`.`laps` AS `laps`,
+        `r`.`status` AS `status`,
+        `r`.`discarded` AS `discarded`,
+        `r`.`points` AS `points`,
+        `r`.`id` AS `race_id`,
+        `e`.`rank` AS `series_rank`,
+        `e`.`final_score` AS `series_score`,
+        `ra`.`date` AS `date`,
+        `cl`.`name` AS `club_name`
+    FROM
+        (
+            (
+                (
+                    (
+                        (
+                            (
+                                (
+                                    (
+                                        `results` `r`
+                                    JOIN `entries` `e` ON
+                                        (`r`.`entry_id` = `e`.`id`)
+                                    )
+                                JOIN `competitors` `c` ON
+                                    (`c`.`id` = `e`.`competitor_id`)
+                                )
+                            JOIN `races` `ra` ON
+                                (`ra`.`id` = `r`.`race_id`)
+                            )
+                        JOIN `series` `s` ON
+                            (`s`.`id` = `ra`.`series_id`)
+                        )
+                    JOIN `clubs` `cl` ON
+                        (`cl`.`id` = `s`.`club_id`)
+                    )
+                JOIN `classifications` `cs` ON
+                    (`cs`.`id` = `e`.`classification_id`)
+                )
+            JOIN `crafts` `cr` ON
+                (`cr`.`id` = `r`.`craft_id`)
+            )
+        JOIN `fleets` `f` ON
+            (`f`.`id` = `e`.`fleet_id`)
+        )
