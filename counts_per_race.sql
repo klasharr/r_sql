bf3fb555f2ff4cc74a9dc023e49c9426f4a6703c
Klaus Harris <klasharr@users.noreply.github.com>
2021-11-14 21:01:51 +0000

    Create counts_per_race.sql
    This gets counts per fleet per race. Combined races are not de-duplicated.

---
+++ counts_per_race.sql
@@ -0,0 +1,14 @@
+SELECT
+        DATE,
+        fleet_name,
+        series_name,
+        race_name,
+        COUNT(*) as count
+    FROM
+        `helm_data_table`
+    WHERE status not in ('DNC','OOD') 
+GROUP BY
+    series_name,
+    race_name, 
+    fleet_name 
+ORDER BY `count`  DESC LIMIT 100
